ac9926323c4eba89da3f4b7765ba8f650c6345dd
mac <3088663751@qq.com>
2026-01-25 17:32:53 +0800

    实现统计连续签到次数功能

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -14,6 +14,7 @@ import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
 import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.BitFieldSubCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +24,7 @@ import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -108,6 +110,47 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         //写入Redis BitMap
         stringRedisTemplate.opsForValue().setBit(key, dayOfMonth - 1, true);
         return Result.ok();
+    }
+
+    @Override
+    public Result signCount() {
+        //获取当前用户
+        Long userId = UserHolder.getUser().getId();
+        //获取当前年月
+        LocalDateTime now = LocalDateTime.now();
+        //拼接key
+        String keySuffix = now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+        String key = USER_SIGN_KEY + userId + keySuffix;
+        //获取当前是这个月的第几天
+        int dayOfMonth = now.getDayOfMonth();
+        //获取本月签到记录，截止至今天
+        List<Long> result = stringRedisTemplate.opsForValue().bitField(
+                key,
+                BitFieldSubCommands.create()
+                        .get(BitFieldSubCommands.BitFieldType.unsigned(dayOfMonth)).valueAt(0)
+        );
+        if(result == null || result.isEmpty()){
+            //没有签到结果
+            return Result.ok(0);
+        }
+        Long num = result.get(0);
+        if(num == 0 || num == null){
+            return Result.ok(0);
+        }
+
+        //计算连续签到算法
+        int count = 0;
+        while (true){
+            if((num & 1) == 0){
+                break;
+            }else {
+                count++;
+            }
+
+            num >>>= 1;
+        }
+
+        return Result.ok(count);
     }
 
     private User createUserWithPhone(String phone){
